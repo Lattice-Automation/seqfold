e14a5b2d0a9c46853cde5b496cd0a943140ac0be
JJTimmons <joshua.timmons1@gmail.com>
2020-01-28 12:28:46 -0500

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except VersionConflict:
 
 setup(
     name="seqfold",
-    version="0.7.0",
+    version="0.7.1",
     description="Predict the minimum free energy structure of nucleic acids",
     author="JJTimmons",
     author_email="jtimmons@latticeautomation.com",
