6be8a52b50009d951eabc874a6dcaed1072a270e
Jonas Hagenberg <38041374+jonas-hag@users.noreply.github.com>
2025-07-09 22:16:06 +0200

    remove `pkg_resources` dependency to determine the version number in __init__.py

--- seqfold/__init__.py
+++ seqfold/__init__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
+from importlib.metadata import version, PackageNotFoundError
 
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = version(dist_name)
+except PackageNotFoundError:
     __version__ = "unknown"
 finally:
-    del get_distribution, DistributionNotFound
+    del version, PackageNotFoundError
 
 from .fold import fold, dg, dg_cache, fold, Struct, dot_bracket
 from .tm import tm, tm_cache, gc_cache
